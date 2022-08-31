6f936c9427500fb0a4c1ca7746dcf13556a24d15
luka2210 <lukamomcilovic221099@gmail.com>
2022-08-31 22:16:01 +0200

    kreiran projekat

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+def main():
+    keras.Sequential()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
